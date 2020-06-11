4cdb9ba3a222e27d0c7a84da097adb978273608e
vinodh1988 <k.c.vinodh@hotmail.com>
2020-06-11 21:15:33 +0530

    first commit

---
+++ second.js
@@ -0,0 +1,21 @@
+const appname="My First app"
+
+
+const temporary=function(){
+    console.log("funny....!!!!");
+}
+
+const names= ["Ram","Ravi","Raj"];
+
+a="Raj"
+
+
+module.exports= {
+    app: appname,
+    fun: temporary,
+    names: names
+}
+
+module.exports.onemore=function(){
+    console.log("ONE MORE")
+}
